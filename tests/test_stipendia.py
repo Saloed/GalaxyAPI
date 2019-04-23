716ab329cfee4c17580d8ba2821ac5a071fed405
Seliverstov-S-A <43793423+Seliverstov-S-A@users.noreply.github.com>
2019-04-24 00:09:24 +0300

    Make request from web

--- tests/test_stipendia.py
+++ tests/test_stipendia.py
@@ -1,11 +1,13 @@
 import json
+import requests
 from jsonschema import validate
 
 
 def test_valid_schema_stipendia():
-    json_data = json.load(open("stipends.ex.json"))
+    #json_data = json.load(open("stipends.ex.json"))
+    t=requests.get("http://localhost:8080/stipends.ex.json")
     schema = json.load(open("stipendia.json"))
-    validate(json_data , schema)
+    validate(t.json() , schema)
 
 if __name__ =="__main__":
     print (test_valid_schema_stipendia())
