16a92845c121d53f670c48bfa1ec1c3650ed4fa5
Seliverstov-S-A <seliverstov_s_a@mail.ru>
2019-06-15 00:15:15 +0000

    Added test for students request

--- tests/test_stipendia.py
+++ tests/test_stipendia.py
@@ -3,9 +3,9 @@ from django.test import Client
 from jsonschema import validate
 from api.views import *
 
-def test_valid_schema_stipendia():
+def test_valid_schema_students():
     client = Client()
-    response = client.get('/api/json/ping/')
+    response = client.get('api/json/students?id=1')
     print(response.content )
-    schema = json.load(open("tests/pingpong.json"))
+    schema = json.load(open("tests/student_schema.json"))
     validate(json.loads(response.content) , schema)
